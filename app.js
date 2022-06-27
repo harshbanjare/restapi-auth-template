f6f36b74b217b14d71c648e21f67272c248f0f00
Harsh <mail@harshbanjare.me>
2022-06-27 23:14:55 +0530

    Initialized firebase and implemented /user/create route.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from "body-parser";
 
 import userRouter from './routes/user.js'
 
 const app = express();
 
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//Handling CORS
 
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if (req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 app.use('/user',userRouter)
 
---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
+
+import serviceAccount from './serviceAccount.js';
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+
+
+export default db;
