a86293867508bd7fd7789c7c1a7cb0b222139716
Harsh <mail@harshbanjare.me>
2022-06-29 18:17:15 +0530

    Implemented CheckAuth middleware for route authorisation

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import create from './user/create.js'
 import login from './user/login.js'
 import checkAuth from "../middleware/check-auth.js";
@@ -7,7 +6,7 @@ import checkAuth from "../middleware/check-auth.js";
 const router = express.Router();
 
 router.get("/", checkAuth, async (req, res, next)=>{
-    res.status(200).json({response:"ok"});
+    res.status(200).json({response:"ok",...req.body.userData});
 })
 
 router.post("/create",create);
