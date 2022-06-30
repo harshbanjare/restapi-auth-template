eb031cd8472e4f318f3a4cf9f9b200f6afe5cdbb
harshbanjare <harsh.12110689@lpu.in>
2022-06-30 12:33:48 +0530

    Dockerized

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import 'dotenv/config'
 import http from 'http';
 import app from "./app.js"
 
-const PORT = process.env.PORT || 3001; //Default Port
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
 
 const server = http.createServer(app);
 
-server.listen(PORT)
+console.log(`Starting server on ${HOST}:${PORT}`);
+server.listen(PORT, HOST);
 
 
